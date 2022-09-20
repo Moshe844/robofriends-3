27206dbc96699d0336b9f29838cf4f4af38c7ed2
MOshea <arye6700@gmail.com>
2022-09-20 17:00:12 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { searchRobots, requestRobots } from "./reducers";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  combineReducers,
+} from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
 
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
